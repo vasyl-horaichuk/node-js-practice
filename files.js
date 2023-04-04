867bb3b254cb9e75a228761d71341b03dfd403ec
Vasyl96 <93982881+vasyl-horaichuk@users.noreply.github.com>
2023-04-04 22:27:41 +0300

    update

--- files.js
+++ files.js
@@ -1,3 +1,13 @@
 const fs = require('fs/promises');
 const path = require('path');
 const chalk = require('chalk');
+
+
+const createFile = (filename, content) => { 
+    const data = {
+        filename, content
+    }
+
+}
+
+module.exports = { createFile };
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello world!")
+const yargs = require('yargs').argv;
+const { createFile } = require('./files');
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      break;
+
+    case "get":
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
